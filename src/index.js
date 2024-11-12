c64523b0054dc44c5fbc51bc377761f7a337fbbc
kottaips <kottaips2020@gmail.com>
2024-11-12 12:00:15 +0530

    First Commit aws code

---
+++ src/index.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import App from './App';
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
+
+// git remote add origin https://github.com/kottaips/ganesh_aws_amplify.git
+
+// git add . 
+// git branch -M main
+// git push -u origin main
